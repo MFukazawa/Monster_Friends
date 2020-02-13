637ef3bdd69430974deb4da7109ab88c7374cf70
Micah Fukazawa <micahfukazawa@gmail.com>
2020-02-13 22:38:35 +0900

    Updates

---
+++ precache-manifest.98ec211731ee81e0c43d07a155a6ac0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd2e7c531fce76472e56445a414155e",
+    "url": "/Monster_Friends/index.html"
+  },
+  {
+    "revision": "90154f0fac35c6ede9c1",
+    "url": "/Monster_Friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "95e5e5c3af8aed059ec2",
+    "url": "/Monster_Friends/static/css/main.c49889d9.chunk.css"
+  },
+  {
+    "revision": "90154f0fac35c6ede9c1",
+    "url": "/Monster_Friends/static/js/2.af5ed0d7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Monster_Friends/static/js/2.af5ed0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e5e5c3af8aed059ec2",
+    "url": "/Monster_Friends/static/js/main.788905df.chunk.js"
+  },
+  {
+    "revision": "fa507e990323853b2b61",
+    "url": "/Monster_Friends/static/js/runtime-main.8bd26739.js"
+  },
+  {
+    "revision": "80f39b150339d82df7f726962de8c143",
+    "url": "/Monster_Friends/static/media/Monsters Attack.80f39b15.woff"
+  }
+]);
